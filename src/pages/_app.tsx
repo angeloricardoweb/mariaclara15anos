a40b9470dab85c93c148339cb0c24031a07e478f
Angelo Ricardo <angeloengcomp@gmail.com>
2023-06-01 01:37:32 -0300

    feat: add seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,35 +5,13 @@ import 'swiper/css/bundle'
 import '../styles/index.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  
+
   return (
     <>
       <DefaultSeo
-        title="_____SiteName_____"
-        description="___SiteDescription___    "
-        canonical="https://vercel.app/"
-        openGraph={{
-          url: 'https://vercel.app/',
-          title: '_____SiteName_____',
-          description: '___SiteDescription___',
-          images: [
-            {
-              url: '/img/seo.png',
-              width: 1200,
-              height: 627,
-              alt: '_____SiteName_____',
-              type: 'image/png',
-            },
-          ],
-          siteName: 'gilmarcasepatentes.com.br',
-        }}
-        twitter={{
-          handle: '@GilmarcaEPatentes',
-          site: '@site',
-          cardType: 'summary_large_image',
-        }}
+        title="Maria Clara 15 Anos"
+        description="Venha comemorar comigo"
       />
-
       <Component {...pageProps} />
     </>
   )
