ed324ec36cc3c052b9d264d4f35e26216169a171
Angelo Ricardo <angeloengcomp@gmail.com>
2023-07-02 19:59:15 -0300

    feat: add validation when access in desktop

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,19 @@ import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
 import 'swiper/css/bundle'
 import '../styles/index.scss'
+import { useEffect, useState } from 'react'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   return (
     <>
@@ -12,7 +23,13 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         title="Maria Clara 15 Anos"
         description="Venha comemorar comigo"
       />
-      <Component {...pageProps} />
+      {isMobile ? (
+        <Component {...pageProps} />
+      ) : (
+        <div className="flex justify-center items-center h-screen">
+          <h1 className="text-3xl text-center">Acesse pelo Celular</h1>
+        </div>
+      )}
     </>
   )
 }
